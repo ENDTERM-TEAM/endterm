425071592ee075a5993edbb1f69a31a2ce270f84
AyabekA <79572748+AyabekA@users.noreply.github.com>
2021-03-07 03:18:45 +0600

    Update Mentors.java

--- src/com/company/entities/Mentors.java
+++ src/com/company/entities/Mentors.java
@@ -1,16 +1,34 @@
 package com.company.entities;
 
-public class Mentors extends Member{
+public class Mentors extends Member implements IWorker{
     public double salary;
 
     public Mentors(String first_name, String last_name, int age, String email, double salary) {
         super(first_name, last_name, age, email);
-
+        setSalary(salary);
     }
 
     public Mentors(int id, String first_name, String last_name, int age, String email, double salary) {
         super(id, first_name, last_name, age, email);
-
+        setSalary(salary);
     }
 
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String work() {
+        return ", is working as mentor";
+    }
+
+    @Override
+    public String getSalary() {
+        return ", is salary="+salary;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + getSalary() + work() + "\n";
+    }
 }
