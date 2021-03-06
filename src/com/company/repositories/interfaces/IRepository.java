36e4a965235ebaa4ba669c81ed6b95eeb2ce1841
AkzhanTursunbek <79572698+AkzhanTursunbek@users.noreply.github.com>
2021-03-07 02:24:13 +0600

    Update IRepository.java

--- src/com/company/repositories/interfaces/IRepository.java
+++ src/com/company/repositories/interfaces/IRepository.java
@@ -4,6 +4,10 @@ import com.company.entities.Students;
 
 public interface IRepository {
     boolean addStudent(Students student);
+    boolean addTeacher(Teachers teachers);
+    ArrayList showAllStudents();
+    boolean removeStudentByID(int id);
+    ArrayList showTop10Students();
     Students getStudentByHighestScore();
     boolean changeEmailById(int id, String email, String table);
 }
