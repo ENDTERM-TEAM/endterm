7e36f8ecaf1391b440fd16caf3dd7a745d5059bf
AkzhanTursunbek <79572698+AkzhanTursunbek@users.noreply.github.com>
2021-03-07 03:21:35 +0600

    Update Groups.java

--- src/com/company/entities/Groups.java
+++ src/com/company/entities/Groups.java
@@ -6,6 +6,15 @@ public class Groups {
     private String level;
     private int mentor_id;
 
+    public Groups () {}
+
+    public Groups (int g_id, String name, String level, int mentor_id) {
+        setG_id(g_id);
+        setName(name);
+        setLevel(level);
+        setMentor_id(mentor_id);
+    }
+
     public int getG_id() {
         return g_id;
     }
@@ -36,5 +45,10 @@ public class Groups {
 
     public void setMentor_id(int mentor_id) {
         this.mentor_id = mentor_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Group " + getG_id() + ", with subject " + getName() + ", level is " + getLevel() + ", mentor: ";
     }
 }
