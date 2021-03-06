cd119557b4341cd3bcc18850fdca1ffb2fa5ee8c
AyabekA <79572748+AyabekA@users.noreply.github.com>
2021-03-07 01:53:23 +0600

    Update IRepository.java

--- src/com/company/repositories/interfaces/IRepository.java
+++ src/com/company/repositories/interfaces/IRepository.java
@@ -3,6 +3,7 @@ package com.company.repositories.interfaces;
 import com.company.entities.Students;
 
 public interface IRepository {
-
-    public boolean addStudent(Students student);
+    boolean addStudent(Students student);
+    Students getStudentByHighestScore();
+    boolean changeEmailById(int id, String email, String table);
 }
