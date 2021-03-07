1e8ced9719f0565e61704c353b397b6bfe5bb6f6
AkzhanTursunbek <79572698+AkzhanTursunbek@users.noreply.github.com>
2021-03-07 19:43:18 +0600

    Update Mentors.java

--- src/com/company/entities/Mentors.java
+++ src/com/company/entities/Mentors.java
@@ -5,21 +5,21 @@ import com.company.entities.interfaces.IWorker;
 public class Mentors extends Member implements IWorker{
     public double salary;
 
-    public Mentors(String first_name, String last_name, int age, String email, double salary) {
+    public Mentors(String first_name, String last_name, int age, String email, double salary) {                            //creating constructors
         super(first_name, last_name, email,age);
         setSalary(salary);
     }
 
-    public Mentors(int id, String first_name, String last_name, int age, String email, double salary) {
+    public Mentors(int id, String first_name, String last_name, int age, String email, double salary) {                    //creating constructors
         super(id, first_name, last_name, email,age);
         setSalary(salary);
     }
 
-    public void setSalary(double salary) {
+    public void setSalary(double salary) {                                                                                  //setter
         this.salary = salary;
     }
 
-    public double getSalary() {
+    public double getSalary() {                                                                                             //getter
         return salary;
     }
 
@@ -33,8 +33,8 @@ public class Mentors extends Member implements IWorker{
         return ", is salary="+salary;
     }
 
-    @Override
+    @Override                                                                                                               //toString method to show all variables
     public String toString() {
         return super.toString() + getSalaries() + work() + "\n";
-    }
+    } 
 }
