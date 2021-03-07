333e5b06b07e37a6c0c54cac93a2423414551b99
AkzhanTursunbek <79572698+AkzhanTursunbek@users.noreply.github.com>
2021-03-07 12:51:51 +0600

    Update IRepository.java

--- src/com/company/repositories/interfaces/IRepository.java
+++ src/com/company/repositories/interfaces/IRepository.java
@@ -1,13 +1,16 @@
 package com.company.repositories.interfaces;
 
 import com.company.entities.Students;
-
+import com.company.entities.Groups;
+import com.company.entities.Mentors;
+import com.company.entities.Students;
+import com.company.entities.Teachers;
 public interface IRepository {
-    boolean addStudent(Students student);
+    boolean addStudent(Students students);
     boolean addTeacher(Teachers teachers);
-    ArrayList showAllStudents();
+    ArrayList<Students> showAllStudents();
     boolean removeStudentByID(int id);
-    ArrayList showTop10Students();
+    ArrayList<Students> showTop10Students();
     Students getStudentByHighestScore();
     boolean changeEmailById(int id, String email, String table);
     ArrayList<Groups> showAllGroup();
