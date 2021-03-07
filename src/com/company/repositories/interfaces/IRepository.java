41cd9db0de22e2d213946df3f0077adb7c3e2dbd
AkzhanTursunbek <79572698+AkzhanTursunbek@users.noreply.github.com>
2021-03-07 19:33:36 +0600

    Update IRepository.java

--- src/com/company/repositories/interfaces/IRepository.java
+++ src/com/company/repositories/interfaces/IRepository.java
@@ -8,10 +8,10 @@ import com.company.entities.Teachers;
 import java.util.ArrayList;
 
 public interface IRepository {
-    boolean addStudent(Students students);
+    boolean addStudent(Students students); //in interface we write what the classes can do
     boolean addTeacher(Teachers teachers);
     ArrayList<Students> showAllStudents();
-    boolean removeStudentByID(int id);
+    boolean removeStudentByID(int id);           //defines a set of common behaviors. 
     ArrayList<Students> showTop10Students();
     Students getStudentByHighestScore();
     boolean changeEmailById(int id, String email, String table);
