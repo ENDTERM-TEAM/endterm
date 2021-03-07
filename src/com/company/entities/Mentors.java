476a5770014231bb1feac671af20c17fc2f475b4
AkzhanTursunbek <79572698+AkzhanTursunbek@users.noreply.github.com>
2021-03-07 13:40:04 +0600

    Update Mentors.java

--- src/com/company/entities/Mentors.java
+++ src/com/company/entities/Mentors.java
@@ -4,23 +4,23 @@ public class Mentors extends Member implements IWorker{
     public double salary;
 
     public Mentors(String first_name, String last_name, int age, String email, double salary) {
-        super(first_name, last_name, age, email);
+        super(first_name, last_name, email,age);
         setSalary(salary);
     }
 
     public Mentors(int id, String first_name, String last_name, int age, String email, double salary) {
-        super(id, first_name, last_name, age, email);
+        super(id, first_name, last_name, email,age);
         setSalary(salary);
     }
 
     public void setSalary(double salary) {
         this.salary = salary;
     }
-    
+
     public double getSalary() {
         return salary;
     }
-    
+
     @Override
     public String work() {
         return ", is working as mentor";
